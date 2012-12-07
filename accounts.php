54181e22d303d49e73aa026168d4ba47bde45b51
Greg Clark <clargr1@umbc.edu>
2012-12-06 21:44:13 -0500

    changed id fields to name fields on input tags, for login form

--- accounts.php
+++ accounts.php
@@ -1,7 +1,7 @@
 ﻿<?php include './models/Account.php';
 	$email = $_POST['inputEmail'];
 	$pass  = $_POST['inputPassword'];	
-
+	
 	/* Connect to database */
 	$con = mysql_connect("studentdb.gl.umbc.edu","clargr1","clargr1") or die("Could not connect to MySQL");
 	$rs = mysql_select_db("clargr1", $con) or die("Could not connect select $con database");
@@ -21,8 +21,6 @@
 	} while($row = mysql_fetch_array($result));
 
 	$db = new DatabaseLink();
-	$a = Account::dbGetByEmail($email, $db);
-	$correctPwd = $a->checkPwd($pass);
 	$correctPwd = Account::dbCheckPwd($email, $pass, $db);
 
 	echo("<p>$inTable<br>$correctPwd</p>");
