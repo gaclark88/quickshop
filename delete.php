dbffb9f29294a7da50ac0bc8f2208b21ea67f1a3
Kevin Schmincke <kschmin1@umbc.edu>
2012-12-07 20:39:02 -0500

    added delete functionality

---
+++ delete.php
@@ -0,0 +1,123 @@
+﻿<?php include "session.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Front Page</title>
+
+    <!--Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 15px;
+        padding-bottom: 20px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+    
+    <!-- Icons -->
+    <link rel="shortcut icon" href="http://twitter.github.com/bootstrap/assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+
+  <body>
+    <!--Start of Center Section-->
+    <div id="center-section">
+
+        <!--Start of Navigation Bar-->
+        <div class="navbar navbar-inverse ">
+            <div class="navbar-inner">
+                <div class="container-nav">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <!--Links to Account Login and Cart-->
+                    <div class="nav-collapse collapse">
+                        <p class="navbar-text pull-right">
+                        <ul class="nav pull-right">
+			    <?php include './models/Account.php';
+				if ( !isset($_SESSION['accountId']) )
+					echo("<a class=\"brand\" href=\"login.php\">Login/Create Account</a>");
+				else {
+					$db = new DatabaseLink();
+					$a  = Account::dbGet($_SESSION['accountId'], $db);
+
+					echo("<a class=\"brand\" href=\"accountmgr.php\">Hello, " . $a->first_name . "!</a>");
+				}
+			    ?>	
+                            <a class="brand" href="#">My Cart</a>
+                        </ul>
+                        </p>
+                    </div>
+                    <!--Search Bar-->
+                    <form class="form-search" action="search.php" method="post" name="Search">
+                        <div style="text-align:left">
+                            <input type="text" name="fsearch" maxlength="100" class="span6  input-large search-query">
+                            <button type="submit" class="btn">Search</button>
+                        </div>
+                    </form><!--End of Search Bar-->
+                </div>
+            </div>
+        </div><!--End of Navigation Bar-->
+
+        <!--Start of the Center Section below the Navigation Bar-->
+        <div class="container-center">
+            <div class="row-fluid">
+                <!--Logo Here-->
+                <a class="brand" href="index.php"> <img src="assets/img/logo.png"></a>
+            </div>
+        
+            <!--Start of Sidebar-->
+            <div class="row-fluid">
+                <div class="span3">
+                    <div class="well sidebar-nav">
+                        <ul class="nav nav-list">
+                            <li class="nav-header">Product Categories</li>
+                            <?php include 'sidebar.php';?>
+                        </ul>
+                    </div><!--End of Sidebar-->
+                </div><!--Span-->
+        
+                <!--Start of Main Section-->
+                <div class="span9">
+                    <div class="container-main">
+
+		    <h3>Are you sure you want to delete your account?</h3>
+		    <p>This cannot be undone</p>
+		    <p>
+			<a class="btn btn-large" type="button" href="del.php">Yes, delete my account forever</a>
+			<a class="btn btn-large" type="button" href="accountmgr.php">No, I want to keep my account</a>
+		    </p>
+                    </div><!--End of Main Section-->
+                </div><!--Span-->
+            </div><!--End of row containing sidebar and main section-->
+
+      <hr><!--Breakline before Footer-->
+      <!--Footer-->
+      <footer>
+        <p><a href="contact.php">Contact Us</a></p>
+      </footer>
+
+        </div><!--End of the Center Section below the Navigation Bar-->
+    </div><!--End of Center Section-->
+    
+
+    <!-- Javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery-1.8.2.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+</body></html>
--- pass.php
+++ pass.php
@@ -12,7 +12,7 @@
 		echo("<script>location.href=\"changepass.php?error=1\"</script>");
 	else {
 		if ($newpass != $confirmpass) {
-			echo("<script>location.href\"changepass.php?passerr=1\"</script>");
+			echo("<script>location.href=\"changepass.php?passerr=1\"</script>");
 		} else {
 			$a->fields["password"] = Account::hashPassword($newpass);
 
